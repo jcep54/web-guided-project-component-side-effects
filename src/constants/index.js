627eea01c0a5243c3ed379a9defe0f8c03f08530
Jose Cepero <ceperojose54@gmail.com>
2023-05-04 16:24:57 -0400

    followed guided instructions

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,7 @@
 export const BASE_URL = 'http://localhost:4001'
 export const API_KEY = 'xyz'
+
+
+// Production / Prod   -> Final product that is open to the world!
+// Stage               -> Thsi is interal but open to others (others see and test code)
+// Development / Dev   -> This is you! localhost (you create code)
